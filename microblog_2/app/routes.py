43451c4827a6d126d40063c17a10465b2e74ddc6
quest4next <anilquest4@gmail.com>
2018-08-25 11:49:54 +0530

    Add microblog part 1 and part 2

---
+++ microblog_2/app/routes.py
@@ -0,0 +1,11 @@
+
+from app import app
+from flask import render_template
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Anil'}
+    return render_template('index.html', user=user) #title='Home', 
+    
